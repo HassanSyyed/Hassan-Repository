54effa39805800439eade0dfa82930a128534464
Hassan imam <hassan.imam@hnl.tv>
2024-10-22 12:48:41 +0500

    first commit

---
+++ src/app/footer.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Footer() {
+    return (
+        <div>
+
+        <footer className="bg-blue-900 text-blue-300 text-center py-4 w-full font-bold mt-10">
+            <p>Created by Hassan Imam</p>
+            
+        </footer>
+        
+        </div>
+    );
+}
