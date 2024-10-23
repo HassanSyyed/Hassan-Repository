84d5ba7d16df33f5526eb45a8bf5274ad49ca837
Syed Hassan Imam <hassan.syyed@yahoo.com>
2024-10-23 14:11:06 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,16 @@
+import Footer from "./componants/footer";
+import Grid from "./componants/grid";
+import Navbar from "./componants/navbar";
+
+export default function Home() {
+
+  return (
+    <div>
+      <Footer/>
+      <Grid/>
+      <Navbar/>
+    </div>
+  );
+}
+
+               
