cc2e062710cdb1bce5848282a602541222c8a3aa
Syed Hassan Imam <hassan.syyed@yahoo.com>
2024-10-23 14:08:42 +0500

    Add files via upload

--- grid.tsx
+++ grid.tsx
@@ -3,8 +3,7 @@ import Image from "next/image";
 export default function Grid() {
   return (
     <div>
-               <div className="grid grid-cols-4 grid-rows-3 gap-1 w-40 h-44">
-               
+               <div className="grid grid-cols-4 grid-rows-3 gap-1 m-4 h-44">  
                <div className="row-span-3 bg-sky-400"></div>
                <div className="col-span-2 bg-red-400"></div>
                <div className="bg-teal-400"></div>
